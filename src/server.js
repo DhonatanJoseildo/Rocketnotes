0c3b929dbb3ae7fa29802c709b7b867257b9ba55
Dhonatan Joseildo <dhon.joseildo@gmail.com>
2022-12-04 23:07:14 -0300

    beginning of the client and server junction

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./Utils/AppError");
 const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
